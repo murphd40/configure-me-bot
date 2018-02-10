29560e89cfa891c6f6bdca01d55f9760c2b6c0f4
David <murphd40@gmail.com>
2018-02-10 01:05:02 +0000

    🌅

---
+++ src/test/java/com/murphd40/configuremebot/ConfigureMeBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.murphd40.configuremebot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigureMeBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
