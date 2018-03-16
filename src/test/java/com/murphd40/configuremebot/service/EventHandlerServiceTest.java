1b1c11b8f50877d5633cb7a91417030fe032a692
David Murphy <DMurphy3@ie.ibm.com>
2018-03-16 23:16:25 +0000

    more tests for EventHandleService

--- src/test/java/com/murphd40/configuremebot/service/EventHandlerServiceTest.java
+++ src/test/java/com/murphd40/configuremebot/service/EventHandlerServiceTest.java
@@ -1,6 +1,7 @@
 package com.murphd40.configuremebot.service;
 
 import java.util.Collections;
+import java.util.UUID;
 
 import com.murphd40.configuremebot.controller.request.MessageCreatedEvent;
 import com.murphd40.configuremebot.dao.model.Trigger;
@@ -13,16 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 
 public class EventHandlerServiceTest {
 
     @InjectMocks
     private EventHandlerService eventHandlerService = new EventHandlerService();
 
-    @Spy
+    @Mock
     private TriggerActions triggerActions;
 
     @Mock
@@ -34,16 +35,81 @@ public class EventHandlerServiceTest {
     }
 
     @Test
-    public void test() {
-        Trigger trigger = new Trigger();
-        trigger.setAction("actions.sendMessage('Daylight come and I wanna go home ' + event.type)");
+    public void executeAction() {
+        Trigger trigger = createTrigger();
 
         Mockito.when(triggerRepository.findBySpaceIdAndEventType(any(), any())).thenReturn(Collections.singletonList(trigger));
 
-        MessageCreatedEvent event = new MessageCreatedEvent();
-        event.setType(EventType.MESSAGE_CREATED.getValue());
+        MessageCreatedEvent event = createEvent();
 
         eventHandlerService.processWebhookEvent(event);
+
+        Mockito.verify(triggerActions).sendMessage(Mockito.eq("Hello world"));
+    }
+
+    @Test
+    public void conditionPasses() {
+        Trigger trigger = createTrigger();
+        trigger.setCondition("event.content.contains('world')");
+
+        Mockito.when(triggerRepository.findBySpaceIdAndEventType(any(), any())).thenReturn(Collections.singletonList(trigger));
+
+        MessageCreatedEvent event = createEvent();
+
+        eventHandlerService.processWebhookEvent(event);
+
+        Mockito.verify(triggerActions).sendMessage(anyString());
+    }
+
+    @Test
+    public void conditionFails() {
+        Trigger trigger = createTrigger();
+        trigger.setCondition("event.content.contains('unicorn')");
+
+        Mockito.when(triggerRepository.findBySpaceIdAndEventType(any(), any())).thenReturn(Collections.singletonList(trigger));
+
+        MessageCreatedEvent event = createEvent();
+
+        eventHandlerService.processWebhookEvent(event);
+
+        Mockito.verify(triggerActions, Mockito.never()).sendMessage(anyString());
+    }
+
+    @Test
+    public void noTriggersForEvent() {
+        Mockito.when(triggerRepository.findBySpaceIdAndEventType(any(), any())).thenReturn(Collections.emptyList());
+
+        eventHandlerService.processWebhookEvent(createEvent());
+
+        Mockito.verify(triggerActions, Mockito.never()).sendMessage(anyString());
+    }
+
+    @Test
+    public void eventTypeNotSupported() {
+        Trigger trigger = createTrigger();
+
+        Mockito.when(triggerRepository.findBySpaceIdAndEventType(any(), any())).thenReturn(Collections.singletonList(trigger));
+
+        MessageCreatedEvent event = createEvent();
+        event.setType("unknown-type");
+
+        eventHandlerService.processWebhookEvent(event);
+
+        Mockito.verify(triggerActions, Mockito.never()).sendMessage(Mockito.anyString());
+    }
+
+    private Trigger createTrigger() {
+        Trigger trigger = new Trigger();
+        trigger.setAction("actions.sendMessage(event.content)");
+        return trigger;
+    }
+
+    private MessageCreatedEvent createEvent() {
+        MessageCreatedEvent event = new MessageCreatedEvent();
+        event.setType(EventType.MESSAGE_CREATED.getValue());
+        event.setContent("Hello world");
+        event.setSpaceId(UUID.randomUUID().toString());
+        return event;
     }
 
 }
